77b57a791c2b1d800043ae0d82fb536ff48a64b6
nujuat <nujuat@gmail.com>
2020-12-05 16:03:58 +1100

    Created alex's scene

---
+++ Assets/Scripts/OrbitScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitScript : MonoBehaviour
+{
+    public float speed = 5;
+    public float angularSpeed = 150;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward*Time.deltaTime*speed);
+        transform.Rotate(0, Time.deltaTime*angularSpeed, 0);
+    }
+}
