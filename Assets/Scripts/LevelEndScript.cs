9e787a1b0dc510360c90de0838a2ac9120f62bf4
nujuat <nujuat@gmail.com>
2020-12-08 22:36:48 +1100

    Tried to add end of level

---
+++ Assets/Scripts/LevelEndScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelEndScript : MonoBehaviour
+{
+    public string nextScene;
+
+    private void OnTrigger(Collider other) {
+        SceneManager.LoadScene(nextScene, LoadSceneMode.Single);
+    }
+}
